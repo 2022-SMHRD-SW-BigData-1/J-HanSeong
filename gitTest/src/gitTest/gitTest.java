93c42dc4c2aa8f2a9a80b26863525d0dec87829a
SMHRD <SMHRD@DESKTOP-VKIJMEU>
2022-08-31 10:25:08 +0900

    두번째 커밋입니다.

--- gitTest/src/gitTest/gitTest.java
+++ gitTest/src/gitTest/gitTest.java
@@ -5,6 +5,8 @@ public class gitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("첫번째 커밋");
+		//두번째 커밋 출력문 만든뒤 --> 해당 내용을 깃 허브 사이트로 올려주세요(push!)
+		System.out.println("두번째 커밋");
 	}
 
 }
